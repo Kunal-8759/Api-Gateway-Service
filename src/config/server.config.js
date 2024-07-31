4bebff38ffe9ed5b123417e2750cfdd7b01aaa1c
Kunal Kumar <kumarkunal8759@gmail.com>
2024-07-31 14:24:55 +0530

    added encryption to the password while signup

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -2,5 +2,6 @@ const dotenv=require('dotenv');
 
 dotenv.config();
 module.exports={
-    PORT:process.env.PORT || 3001
+    PORT:process.env.PORT || 3001,
+    SALT_ROUNDS:process.env.SALT_ROUNDS
 }
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model} = require('sequelize');
+const bcrypt=require('bcrypt');
+const {SALT_ROUNDS}=require('../config/server.config');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +34,11 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate(function encrypt(user){
+    const encryptedPassWord=bcrypt.hashSync(user.password,+SALT_ROUNDS);
+    user.password=encryptedPassWord;
+  })
+
   return User;
 };
